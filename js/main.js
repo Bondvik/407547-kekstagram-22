346fb212d9f01ccaa8eb88c8cdcf037b54ec822e
Viktoriia <bondvik16@gmail.com>
2021-01-30 20:41:51 +0200

    Исправила замечания по ДЗ

--- js/main.js
+++ js/main.js
@@ -4,28 +4,21 @@ const getMaxLengthComment = function (comment) {
   return (comment.length < COMMENT_LENGTH) ? true : false;
 };
 
-
 //Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const getRandomNumber = function(min, max) {
-  let a =  Math.ceil(min);
-  let b = Math.floor(max);
-
+  let firstNumber =  Math.ceil(min);
+  let secondNumber = Math.floor(max);
   if (min < 0 || max < 0) {
     return;
   }
-
   if (min === max) {
     return min;
   }
-
   if (min > max) {
-    a = Math.ceil(max);
-    b = Math.floor(min);
+    firstNumber = Math.ceil(max);
+    secondNumber = Math.floor(min);
   }
-
-  return Math.floor(Math.random() * (b - a + 1)) + a;
-
+  return Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
 };
-
 getMaxLengthComment('absd');
 getRandomNumber(1, 10);
