53bb3f5a86f12bd8a55b4b7dcff3867158d90173
Viktoriia <bondvik16@gmail.com>
2021-02-14 12:28:09 +0200

    Выполнила задание по отображению фотографий других пользователей

---
+++ js/create-picture.js
@@ -0,0 +1,16 @@
+const createPicture = (pictures) => {
+  const pictureListElement = document.querySelector('.pictures');
+  const pictureTemplate = document.querySelector('#picture').content;
+  const pictureListFragment = document.createDocumentFragment();
+  pictures.forEach( (picture) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+    pictureListFragment.appendChild(pictureElement);
+  })
+
+  pictureListElement.appendChild(pictureListFragment);
+}
+
+export {createPicture};
